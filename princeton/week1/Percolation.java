37a601083fcef4c3c09dbe5fd636c3c873a409e8
Dan <pichumy@gmail.com>
2018-07-21 20:59:11 -0700

    Finished percolation, fixing some bugs and switching to boolean type. Starting PercolationStats

--- princeton/week1/Percolation.java
+++ princeton/week1/Percolation.java
@@ -4,28 +4,30 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation{
   private WeightedQuickUnionUF unionStructure;
-  private int[][] store;
+  private boolean[] openedSites;
   private int size; // keeps track of n x n size
   private int openSites; // keeps track of how many sites are open
   // create n by n grid
   public Percolation(int n){
       size = n;
-      unionStructure = new WeightedQuickUnionUF(n * n);
-      store = int[n][n];
-      for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-          store[i][j] = 0; // 0 means closed
-        }
-      }
+      unionStructure = new WeightedQuickUnionUF(n * n + 2);
+      openedSites = new boolean[size * size];
   }
   //open site at row, col if not open already
   public void open(int row, int col){
     outOfBounds(row, col);
     // if closed, open it
     if(!isOpen(row, col)){
-      store[row - 1][col - 1] = 1; // opens inside store
+      openSites += 1;
+      int position = (row - 1) * size + (col - 1) + 1;
+      openedSites[position - 1] = true;
+      if(row == 1){
+        unionStructure.union(0, position);
+      }
+      if(row == size){
+        unionStructure.union(size * size + 2, position);
+      }
       // check above
-      int position = (row - 1) * size + (col - 1);
       if(row != 1 && isOpen(row - 1, col)){
         unionStructure.union(position - size, position);
       }
@@ -47,11 +49,13 @@ public class Percolation{
   // checks if spot is open
   public boolean isOpen(int row, int col){
     outOfBounds(row, col);
-    return (store[row - 1][col - 1] != 0);
+    int position = (row - 1) * size + (col - 1);
+    return openedSites[position];
   }
   // checks if site is full
   public boolean isFull(int row, int col){
-    outOfBounds(row, col);
+    int position = (row - 1) * size + (col - 1) + 1;
+    return unionStructure.connected(0, position);
   }
   // returns how many sites are open
   public int numberOfOpenSites(){
@@ -59,14 +63,13 @@ public class Percolation{
   }
   // does the system percolate?
   public boolean percolates() {
-
+    return unionStructure.connected(0, size * size + 2);
   }
 
   private void outOfBounds(int row, int col){
     if(row <= 0 || col <= 0 || row > size || col > size){
-      throw new IllegalArgumentException("Out of bounds")
+      throw new IllegalArgumentException("Out of bounds");
     }
   }
 
-  public static void main(string[] args) // test client if needed
 }
---
+++ princeton/week1/PercolationStats.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class PercolationStats {
+  // perform trials independent experiments on an n x n grid
+  public PercolationStats(int n, int trials){
+
+  }
+  // sample mean of percolation threshold
+  public double mean(){
+
+  }
+  // sample standard deviation of percolation threshold
+  public double stddev(){
+
+  }
+
+  public double confidenceLo(){
+    
+  }
+}
