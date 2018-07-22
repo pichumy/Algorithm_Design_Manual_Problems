1f110996fdb131fd75085bfb0177439ab2de51f4
Dan <pichumy@gmail.com>
2018-07-21 21:53:40 -0700

    Adjusted formatting to fit princeton standards, fixed percolation off by one error

--- princeton/week1/Percolation.java
+++ princeton/week1/Percolation.java
@@ -2,24 +2,24 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation{
+public class Percolation {
   private WeightedQuickUnionUF unionStructure;
   private boolean[] openedSites;
   private int size; // keeps track of n x n size
   private int openSites; // keeps track of how many sites are open
   // create n by n grid
-  public Percolation(int n){
+  public Percolation(int n) {
       size = n;
       unionStructure = new WeightedQuickUnionUF(n * n + 2);
-      openedSites = new boolean[size * size];
+      openedSites = new boolean[n * n];
   }
-  //open site at row, col if not open already
-  public void open(int row, int col){
+  // open site at row, col if not open already
+  public void open(int row, int col) {
     outOfBounds(row, col);
     // if closed, open it
     if(!isOpen(row, col)){
       openSites += 1;
-      int position = (row - 1) * size + (col - 1) + 1;
+      int position = position();
       openedSites[position - 1] = true;
       if(row == 1){
         unionStructure.union(0, position);
@@ -28,48 +28,50 @@ public class Percolation{
         unionStructure.union(size * size + 2, position);
       }
       // check above
-      if(row != 1 && isOpen(row - 1, col)){
+      if(row != 1 && isOpen(row - 1, col)) {
         unionStructure.union(position - size, position);
       }
       // check below
-      if(row != size && isOpen(row + 1, col)){
+      if(row != size && isOpen(row + 1, col)) {
         unionStructure.union(position + size, position);
       }
       // check left
-      if(col != 0 && isOpen(row, col - 1)){
+      if(col != 0 && isOpen(row, col - 1)) {
         unionStructure.union(position - 1, position);
       }
       // check right
-      if( col != size && isOpen(row, col + 1)){
+      if( col != size && isOpen(row, col + 1)) {
         unionStructure.union(position + 1, position);
       }
     }
 
   }
   // checks if spot is open
-  public boolean isOpen(int row, int col){
+  public boolean isOpen(int row, int col) {
     outOfBounds(row, col);
-    int position = (row - 1) * size + (col - 1);
-    return openedSites[position];
+    return openedSites[position() - 1];
   }
   // checks if site is full
-  public boolean isFull(int row, int col){
-    int position = (row - 1) * size + (col - 1) + 1;
-    return unionStructure.connected(0, position);
+  public boolean isFull(int row, int col) {
+    return unionStructure.connected(0, position());
   }
   // returns how many sites are open
-  public int numberOfOpenSites(){
+  public int numberOfOpenSites() {
       return openSites;
   }
   // does the system percolate?
   public boolean percolates() {
-    return unionStructure.connected(0, size * size + 2);
+    return unionStructure.connected(0, size * size + 1);
   }
 
-  private void outOfBounds(int row, int col){
+  private void outOfBounds(int row, int col) {
     if(row <= 0 || col <= 0 || row > size || col > size){
       throw new IllegalArgumentException("Out of bounds");
     }
+  }
+
+  private position(int row, int col){
+    return (row - 1) * size + (col - 1) + 1;
   }
 
 }
--- princeton/week1/PercolationStats.java
+++ princeton/week1/PercolationStats.java
@@ -5,27 +5,27 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class PercolationStats {
   private double[] results; // keep track of results of each trial
   // perform trials independent experiments on an n x n grid
-  public PercolationStats(int n, int trials){
-    if(n <= 0 || trials <= 0){
+  public PercolationStats(int n, int trials) {
+    if(n <= 0 || trials <= 0) {
       throw new IllegalArgumentException("n and trials must be greater than 0");
     }
     results = new double[trials];
     runTrials(size, trials);
   }
 
-  private void runTrials(int size, int trials){
-    for(int i = 0; i < trials; i++){
+  private void runTrials(int size, int trials) {
+    for(int i = 0; i < trials; i++) {
       results[i] = runTrial(size);
     }
   }
 
-  private double runTrial(int size){
+  private double runTrial(int size) {
     Percolation board = new Percolation(size);
     double spacesUsed = 0;
-    while(!board.percolates()){
+    while(!board.percolates()) {
       int row = StdRandom.uniform(1, size + 1);
       int col = StdRandom.uniform(1, size + 1);
-      if(!board.isOpen(row, col)){
+      if(!board.isOpen(row, col)) {
         board.open(row, col);
         spacesUsed++;
       }
@@ -33,23 +33,27 @@ public class PercolationStats {
     return spacesUsed / (size * size);
   }
   // sample mean of percolation threshold
-  public double mean(){
+  public double mean() {
     // takes an array of numbers and returns the mean
     return StdStats.mean(results);
   }
   // sample standard deviation of percolation threshold
-  public double stddev(){
+  public double stddev() {
       // takes an array of numbers and returns the stddev
       return StdStats.stddev(results);
   }
   // lower bound
-  public double confidenceLo(){
-    // 1.96 is used for a 95% confidence interval
-    return mean() - (1.96 * stddev() / Math.sqrt(results.length));
+  public double confidenceLo() {
+    return mean() - confidenceMiddle();
   }
   // upper bound
-  public double confidenceHi(){
-    return mean() + (1.96 * stddev() / Math.sqrt(results.length));
+  public double confidenceHi() {
+    return mean() + confidenceMiddle();
+  }
+
+  private double confidenceMiddle() {
+    // 1.96 is used for a 95% confidence interval
+    return (1.96 * stddev() / Math.sqrt(results.length));
   }
 
   public static void main(String[] args){
